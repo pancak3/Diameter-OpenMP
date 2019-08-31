99597e1f8366981897fe52babd0e83f0df542fa7
Qifan Deng <qifand@student.unimelb.edu.au>
2019-09-01 01:30:49 +1000

    make test_log better

--- check.sh
+++ check.sh
@@ -6,8 +6,11 @@ SEED=7
 gcc headers/randomgraph.c -o randomgraph
 gcc graph-diameter.c -o graph-diameter
 echo "+--------------------------------------+" >> test_log
+git log |grep -E -m 1 "commit ([a-z0-9])+" >> test_log
+
 echo "[*] problem size: $PROBLEM_SIZE" >> test_log
 date +"     %A, %b %d, %Y %I:%M %p" >> test_log
+
 echo "" >> test_log
 echo "[******** onePunch ********]" >> test_log
 gcc -fopenmp onePunch.c -o onePunch >> test_log >> test_log
@@ -19,4 +22,4 @@ echo "[******** origin ********]" >> test_log
 gcc -fopenmp graph-diameter.c -o graph-diameter >> test_log
 ./randomgraph $PROBLEM_SIZE $PROB_EDGE $WEIGHT $SEED | ./graph-diameter >> test_log
 
-tail test_log
+tail test_log -n 12
