e4a25647dadd7bb4367cdfeafeb40a54ceca7965
Qifan Deng <qifand@student.unimelb.edu.au>
2019-09-01 01:01:54 +1000

    added log

--- check.sh
+++ check.sh
@@ -6,15 +6,17 @@ SEED=7
 gcc headers/randomgraph.c -o randomgraph
 gcc graph-diameter.c -o graph-diameter
 
-echo "[*] problem size: $PROBLEM_SIZE"
-date +"     %A, %b %d, %Y %I:%M %p"
-echo ""
-echo "[******** onePunch ********]"
-gcc -fopenmp onePunch.c -o onePunch
-./randomgraph $PROBLEM_SIZE $PROB_EDGE $WEIGHT $SEED | ./onePunch
+echo "[*] problem size: $PROBLEM_SIZE" >> test_log
+date +"     %A, %b %d, %Y %I:%M %p" >> test_log
+echo "" >> test_log
+echo "[******** onePunch ********]" >> test_log
+gcc -fopenmp onePunch.c -o onePunch >> test_log >> test_log
+./randomgraph $PROBLEM_SIZE $PROB_EDGE $WEIGHT $SEED | ./onePunch >> test_log
 
-echo ""
-echo "[******** origin ********]"
+echo "" >> test_log
+echo "[******** origin ********]" >> test_log
 ##./randomgraph 100 0.3 5 1
-gcc -fopenmp graph-diameter.c -o graph-diameter
-./randomgraph $PROBLEM_SIZE $PROB_EDGE $WEIGHT $SEED | ./graph-diameter
+gcc -fopenmp graph-diameter.c -o graph-diameter >> test_log
+./randomgraph $PROBLEM_SIZE $PROB_EDGE $WEIGHT $SEED | ./graph-diameter >> test_log
+
+tail test_log
