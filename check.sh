aaeb3320dc5da72ce6eef183e9c243401cd9f34a
Qifan Deng <qifand@student.unimelb.edu.au>
2019-09-01 13:39:41 +1000

    wrote check.sh to auto test different problem sizes

--- check.sh
+++ check.sh
@@ -1,35 +1,89 @@
-PROBLEM_SIZE=$1
 WEIGHT=1000
 PROB_EDGE=0.343
 SEED=7
 
-gcc -fopenmp getMaxThreads.c -o getMaxThreads
-MAX_THREADS=$(./getMaxThreads)
+one_time_check() {
+  PROBLEM_SIZE=$1
 
-gcc headers/randomgraph.c -o randomgraph
-gcc graph-diameter.c -o graph-diameter
-echo "+--------------------------------------+" >>test_log
-git log | grep -E -m 1 "commit ([a-z0-9])+" >>test_log
+  gcc -fopenmp getMaxThreads.c -o getMaxThreads
+  MAX_THREADS=$(./getMaxThreads)
 
-echo "[*] problem size: $PROBLEM_SIZE, max threads: $MAX_THREADS" >>test_log
-date +"     %A, %b %d, %Y %I:%M %p" >>test_log
+  gcc headers/randomgraph.c -o randomgraph
+  gcc graph-diameter.c -o graph-diameter
 
-echo "" >>test_log
-echo "[******** onePunch ********]" >>test_log
-gcc -fopenmp onePunch.c -o onePunch >>test_log >>test_log
-./randomgraph $PROBLEM_SIZE $PROB_EDGE $WEIGHT $SEED | ./onePunch >>test_log
+  echo -e "$MAX_THREADS,\c" >>log.csv
+  echo -e "$PROBLEM_SIZE,\c" >>log.csv
 
-echo "" >>test_log
-echo "[******** origin-parallel ********]" >>test_log
-##./randomgraph 100 0.3 5 1
-gcc -fopenmp graph-diameter-parallel.c -o graph-diameter-parallel >>test_log
-./randomgraph $PROBLEM_SIZE $PROB_EDGE $WEIGHT $SEED | ./graph-diameter-parallel >>test_log
+  ## get diameter and time cost of onePunch
+  gcc -fopenmp onePunch.c -o onePunch
+  OUTPUT=$(./randomgraph $PROBLEM_SIZE $PROB_EDGE $WEIGHT $SEED |
+    ./onePunch)
+  echo $OUTPUT
 
+  ## write diameter
+  DIAMETER=$(echo $OUTPUT |
+    grep -Po "Diameter [0-9]+" -m 1 |
+    grep -Po "[0-9]+")
+  echo -e "$DIAMETER,\c" >>log.csv
+  ## write time cost
+  TIME_COST=$(echo $OUTPUT |
+    grep -Po "Time: [0-9]+" -m 1 |
+    grep -Po "[0-9]+")
+  echo -e "$TIME_COST,\c" >>log.csv
 
-echo "" >>test_log
-echo "[******** origin ********]" >>test_log
-##./randomgraph 100 0.3 5 1
-gcc -fopenmp graph-diameter.c -o graph-diameter >>test_log
-./randomgraph $PROBLEM_SIZE $PROB_EDGE $WEIGHT $SEED | ./graph-diameter >>test_log
+  ## get diameter and time cost of graph-diameter-parallel
+  gcc -fopenmp graph-diameter-parallel.c -o graph-diameter-parallel
+  OUTPUT=$(./randomgraph $PROBLEM_SIZE $PROB_EDGE $WEIGHT $SEED |
+    ./graph-diameter-parallel)
+  echo $OUTPUT
 
-tail test_log -n 16
+  ## write diameter
+  DIAMETER=$(echo $OUTPUT |
+    grep -Po "Diameter [0-9]+" -m 1 |
+    grep -Po "[0-9]+")
+  echo -e "$DIAMETER,\c" >>log.csv
+  ## write time cost
+  TIME_COST=$(echo $OUTPUT |
+    grep -Po "Time: [0-9]+" -m 1 |
+    grep -Po "[0-9]+")
+  echo -e "$TIME_COST,\c" >>log.csv
+
+  ## get diameter and time cost of graph-diameter
+  gcc -fopenmp graph-diameter.c -o graph-diameter
+  OUTPUT=$(./randomgraph $PROBLEM_SIZE $PROB_EDGE $WEIGHT $SEED |
+    ./graph-diameter)
+  echo $OUTPUT
+
+  ## write diameter
+  DIAMETER=$(echo $OUTPUT |
+    grep -Po "Diameter [0-9]+" -m 1 |
+    grep -Po "[0-9]+")
+  echo -e "$DIAMETER,\c" >>log.csv
+  ## write time cost
+  TIME_COST=$(echo $OUTPUT |
+    grep -Po "Time: [0-9]+" -m 1 |
+    grep -Po "[0-9]+")
+  echo -e "$TIME_COST,\c" >>log.csv
+
+  ## write \r\n to log
+  echo -e "\r" >>log.csv
+}
+
+if [ ! $1 ] || [ ! $2 ] || [ ! $3 ] || [ $1 -gt $2 ]; then
+  echo -e "Usage:\r\n  check.sh \$problem_size_low \$problem_size_high \$problem_size_step\r\n"
+  echo -e "Example:\r\n  check.sh 100 200 50\r\n"
+
+else
+
+  echo "[*] Problem size from"
+  echo "          $1 to $2 by step $3"
+  echo "    Results will be appended in log.csv"
+
+  echo "[*]+--------+"
+  for ((i = $1; i < $2 + 1; i += $3)); do
+    echo "    Testing $i ... "
+    one_time_check $i
+    echo -e "    done.\r\n\r\n"
+  done
+
+fi
